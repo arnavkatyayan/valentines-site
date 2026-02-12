392920c33a437c5c76d9d58df0d429042124927e
Arnav Katyayan <ishumisra99@gmail.com>
2026-02-12 20:31:45 +0530

    First commit.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+   <div className="h-screen flex items-center justify-center bg-pink-200">
+  <div className="text-6xl font-bold text-amber-300">
+    Hi Tanya ❤️
+  </div>
+</div>
+  );
+}
+
+export default App;
+
+
+
